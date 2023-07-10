1943a8a41b04b7f3a2786b312a1c56822d8bc910
ninawtt <ninawu424@gmail.com>
2023-07-10 16:24:06 -0400

    initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def sum(a, b):
+    return a + b
+
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = sum(number1, number2)
+
+print(result)
